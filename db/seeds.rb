e3827470e94a56acc89dc4ec377007479d753c59
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-01-19 21:12:34 +0100

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+lucien = { name: 'Chez Lucien', adress: '63-Trou paumé', category: 'french' }
+chang = { name: 'Chang fu', adress: '38-Cliché raciste', category: 'chinese' }
+luigi = { name: 'Ma que', adress: '69-Encore un cliché', category: 'italian' }
+frite = { name: 'Frite une fois', adress: '75-Accent forcé', category: 'belgian' }
+bruce = { name: 'Yooooh Ahhhh !', adress: '01-Yeux plissés', category: 'japanese' }
+
+[lucien, chang, luigi, frite, bruce].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
